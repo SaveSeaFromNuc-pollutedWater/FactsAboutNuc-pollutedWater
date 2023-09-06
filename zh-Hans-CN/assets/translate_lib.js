e41ca97b88ae7030f53829fcc342d660ce668611
PJ-568 <LiuTian-PJ568@PJ568.eu.org>
2023-09-06 07:20:58 +0000

    Deploying to release from @ SaveSeaFromNuc-pollutedWater/FactsAboutNuc-pollutedWater@31f250e7772e14cdd8fba3ce90f6a50aa424fde2 🚀

---
+++ zh-Hans-CN/assets/translate_lib.js
@@ -0,0 +1,10 @@
+try{
+    translate.listener.start();
+    translate.language.setLocal('chinese_simplified');
+    translate.setAutoDiscriminateLocalLanguage();
+    translate.language.setUrlParamControl();
+}
+catch(e){console.log(e);}
+translate.setUseVersion2();
+translate.ignore.class.push('notTranslate');
+translate.execute();
